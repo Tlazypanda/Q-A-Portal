c120978fb9e2856dcac10ab40b25d4fd66142835
YashRE42 <33805964+YashRE42@users.noreply.github.com>
2018-05-19 16:42:10 +0530

    connected mongoose with db, prepared route for insertion

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
     express = require("express"),
+    Question = require("./models/question"),
     app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine","ejs");
+
+mongoose.connect('mongodb://localhost/Q&AForum',function(){
+    //prints 1 if connection made
+    console.log(mongoose.connection.readyState);});
 
 app.get("/question",function(req,res){
     res.send("Why, hello there!");
-})
+});
+
+app.get("/question/new",function(req,res){
+    res.render("questions/new");
+});
 
 app.listen("2000",function(){
-    console.log("it lives on " + ":2000");
+    console.log("it lives");
 });
