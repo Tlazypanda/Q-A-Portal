f67fdd9b26965bb7139dfc57f89a77ad041a3a8f
Tlazypanda <33183263+Tlazypanda@users.noreply.github.com>
2018-05-20 20:34:54 +0530

    Added answers,likes and basic design

--- app.js
+++ app.js
@@ -37,6 +37,46 @@ app.post("/question",function(req,res){
     });
 });
 
+app.post("/question/like/:id",(req,res) => {
+	 Question.findOne({
+		 _id:req.params.id
+	 })
+	.then(question => {
+		 question.likes=question.likes+1;
+		 
+		 
+		
+		 
+		 question.save().then(question => {
+			
+			 res.redirect("/question");
+		 })
+	 });
+	
+});
+	
+
+
+app.post("/question/answer/:id", (req,res) => { 
+	 Question.findOne({
+		 _id:req.params.id
+	 })
+	.then(question => {
+		 const newAnswer = {
+			 answerBody : req.body.answerBody,
+			 //answerUser : req.user.id
+		 }
+		 
+		 question.answers.unshift(newAnswer);
+		 
+		 question.save().then(question => {
+			
+			 res.redirect("/question");
+		 })
+	 });
+	
+});
+
 app.get("/question", function(req, res){
     Question.find({},function(err,allQuestions){
         if(err)
