d506a31c6ed5f4dede0ee7cf8684624e69e63940
Tlazypanda <33183263+Tlazypanda@users.noreply.github.com>
2018-06-03 01:01:14 +0530

    Revert "Merge pull request #2 from fossiiita/master"
    This reverts commit 00d464431bc2a6174aed9d9dafab0e8b4120c7af, reversing
    changes made to f67fdd9b26965bb7139dfc57f89a77ad041a3a8f.

--- server.js
+++ server.js
@@ -1,99 +1,138 @@
-//Including all the dependencies
 var mongoose = require('mongoose'),
+nev = require('email-verification')(mongoose),
 User=require('./userModel'),
 express=require('express'),
-ejs=require('ejs'),
-session = require('client-sessions'),
-bodyParser=require('body-parser'),
-email='',password='',
-nodemailer = require('nodemailer'),
-smtpTransport = require('nodemailer-smtp-transport');
-//setting app and middlewares
+hbs=require('hbs'),
+bodyParser=require('body-parser'),email='',password='',
+randomstring = require("randomstring"),
+URL=randomstring.generate(10);
 mongoose.connect('mongodb://localhost:27017/user-credentials');
 var app=express();
-app.set('view engines','ejs');
-app.use(session({
-    cookieName: 'session',
-    secret: 'random_string_goes_here',
-    duration: 24* 60 * 60 * 1000,
-    activeDuration: 10 * 60 * 1000,
-  }));
-  //handling requests
+app.set('view engines','hbs');
+
+var TempUser = nev.generateTempUserModel(User);
+
+var options={
+verificationURL: `localhost:3000/land/${URL}`,
+URLLength: 10,
+
+// mongo-stuff
+persistentUserModel: User,
+tempUserModel: TempUser,
+tempUserCollection: 'temporary_users',
+emailFieldName: 'email',
+passwordFieldName: 'password',
+URLFieldName: 'GENERATED_VERIFYING_URL',
+expirationTime: 86400,
+
+// emailing options
+transportOptions: {
+ service: 'Gmail',
+ auth: {
+     user: 'user@gmail.com',
+     pass: 'password'
+ }
+},
+verifyMailOptions: {
+ from: 'Do Not Reply <user@gmail.com>',
+ subject: 'Confirm your account',
+ html: '<p>Please verify your account by clicking <a href="${URL}">this link</a>. If you are unable to do so, copy and ' +
+         'paste the following link into your browser:</p><p>${URL}</p>',
+ text: 'Please verify your account by clicking the following link, or by copying and pasting it into your browser: ${URL}'
+},
+shouldSendConfirmation: true,
+confirmMailOptions: {
+ from: 'Do Not Reply <user@gmail.com>',
+ subject: 'Successfully verified!',
+ html: '<p>Your account has been successfully verified.</p>',
+ text: 'Your account has been successfully verified.'
+}
+};
+
+
+
+
+nev.configure(options, function(error, options){
+if(error)
+return console.log('Unable to Sign Up');
+});
 app.get('/signup',(req,res)=>
 {
-    if (req.session && req.session.user) 
-    { 
-        User.findOne({ email: req.session.user.email }, function (err, user) {
-            if (!user) {
-                req.session.reset();
-                res.redirect('/signup');
-              } else {
-                User.findByIdAndUpdate(  user._id,               
-                    {isVerified:true},
-                    (err, todo) => {
-                        if (err) return res.status(500).send(err);
-                        console.log('Updated user');
-                    }
-                );
-                  console.log(user);
-              res.render('login.ejs');
-              }
-        })
-    }
-    else{
-        res.render('signup.ejs');
-    }
+res.render('login.hbs');
 });
-
-
-
+app.get('/land',(req,res)=>
+{
+res.render('land.hbs');
+});
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.post('/land', urlencodedParser, function (req, res) {
+app.post('/signup', urlencodedParser, function (req, res) {
 if (!req.body) return res.sendStatus(400);
 
 
 email = req.body.email,
 password = req.body.password;
- var isVerified=false;
+var newUser = User({
+ email: email,
+ password: password
+});
+nev.createTempUser(newUser, function(err, existingPersistentUser, newTempUser) {
+ // some sort of error
+ if (err)
+     res.send('Unable to add user');
 
-var userData = {
-    email,
-    password, 
-    isVerified
-  }
+ // user already exists in persistent collection...
+ if (existingPersistentUser)
+     // handle user's existence... violently.
+     res.send('User exists with same id');
+ // a new user
+ if (newTempUser) {
+     var URL = newTempUser[nev.options.URLFieldName];
+     nev.sendVerificationEmail(email, URL, function(err, info) {
+         if (err)
+         res.send("Network failure");
 
+         res.send('Verification email sent');
+     });
 
-
-  //use schema.create to insert data into the db
-  User.create(userData, function (err, user) {
-    if (err) {
-        res.send('User registered with same mail id');
-    } 
-    req.session.user = user;
-    console.log(req.session.user);
-  });
-  
-var transporter = nodemailer.createTransport('smtps://email%40gmail.com:password@smtp.gmail.com');
-var mailOptions = {
-    to:email, // list of receivers
-    subject: 'Signup Confirmation', // Subject line
-    text:'Copy this link to your browser to complete your registration\n\n localhost:3000/signup' // plaintext body
-};
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        console.log(error);
-        res.json({yo: 'error'});
-    }else{
-        console.log('Message sent: ' + info.response);
-       res.render('land.ejs');
-    };
+ // user already exists in temporary collection...
+ } else {
+    res.send('Please verify your account from email');
+ }
 });
 });
 
-app.set('port', (process.env.PORT || 3000));
 
-app.listen(app.get('port'), function(){
-	console.log('Server up on port '+app.get('port'));
-});
 
+// var myHasher = function(password, tempUserData, insertTempUser, callback) {
+//     var hash = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+//     return insertTempUser(hash, tempUserData, callback);
+//   };
+
+//   // async version of hashing function
+//   myHasher = function(password, tempUserData, insertTempUser, callback) {
+//     bcrypt.genSalt(8, function(err, salt) {
+//       bcrypt.hash(password, salt, function(err, hash) {
+//         return insertTempUser(hash, tempUserData, callback);
+//       });
+//     });
+//   };
+//   var url = '...';
+// nev.confirmTempUser(url, function(err, user) {
+//     if (err)
+//         // handle error...
+
+//     // user was found!
+//     if (user) {
+//         // optional
+//         nev.sendConfirmationEmail(user['email_field_name'], function(err, info) {
+//             // redirect to their profile...
+//         });
+//     }
+
+//     // user's data probably expired...
+//     else
+//         // redirect to sign-up
+// });
+
+app.listen(3000);
--- userModel.js
+++ userModel.js
@@ -1,31 +1,24 @@
 var mongoose=require('mongoose');
-var bcrypt=require('bcrypt');
-var UserSchema=new mongoose.Schema({
+var user=mongoose.model('newUser',{
     email:{
         type:String,
-        required:true,
-        unique:true
+        required:true
     },
     password:
     {
         type:String,
-        required:true
+        required:true,
+        minlength:8,
+        maxlength:32
     },
-    isVerified:{
-        type:Boolean,
-        default:false
+    confirmPassword:{
+        type:String,
+        required:true,
+        minlength:8,
+        maxlength:32
     }
 });
-UserSchema.pre('save', function (next) {
-    var user = this;
-    bcrypt.hash(user.password, 10, function (err, hash){
-      if (err) {
-        return next(err);
-      }
-      user.password = hash;
-      next();
-    })
-  });
-var User = mongoose.model('User', UserSchema);
-module.exports=User;
+module.exports={
+    user
+};
 
